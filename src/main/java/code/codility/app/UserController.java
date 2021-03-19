10ab0b5eea7465f6cadb6337ae76c1eb94c16b46
rajkharatmol <rajkharatmol@gmail.com>
2021-03-18 21:00:28 -0400

    Signed-off-by: rajkharatmol <rajkharatmol@gmail.com>

--- src/main/java/code/codility/app/UserController.java
+++ src/main/java/code/codility/app/UserController.java
@@ -1,5 +1,14 @@
 package code.codility.app;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!"; 
+	}
 
 }
--- src/main/java/code/codility/app/UserService.java
+++ src/main/java/code/codility/app/UserService.java
@@ -1,5 +1,7 @@
 package code.codility.app;
 
 public class UserService {
-
+ @SuppressWarnings("unused")
+private String str ="ssds";
+	// calls servicesrtert	
 }
